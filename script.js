49dc276fc8564a1be13443a8fbaf65c4daf2564e
Dawid Dymarkowski <avacade2k@gmail.com>
2019-11-30 15:01:18 +0100

    idk man seems gay to me

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-var city;
+    $(document).ready(function() {
+    $('#city').keyup(function() {
 
-$.getJSON("api.openweathermap.org/data/2.5/weather?q={"+city"}", function(json) {
-    //så man väljer staden av weather report
-}
+       var yourLocation = $("#city").val();
+  $("#debug").append("You are searching for ... "+yourLocation+"\n");
+
+dataString = "London";
+
+$.getJSON('api.openweathermap.org/data/2.5/weather?q=' + dataString, function(json_data){
+alert(JSON.stringify(json_data));
+});
+
+
+});
+});
