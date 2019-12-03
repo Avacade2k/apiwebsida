11b2753010545ccc851255520e02331b2b74ec15
Dawid Dymarkowski <avacade2k@gmail.com>
2019-12-03 10:46:25 +0100

    ez display data omegapope

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
     $(document).ready(function() {
-    $('#city').keyup(function() {
-
+    $(document).on('keypress',function(e) {
+    if(e.which == 13) {
        var yourLocation = $("#city").val();
   $("#debug").append("You are searching for ... "+yourLocation+"\n");
 
@@ -8,8 +8,9 @@ var apikey = "7b1c59b130e4e81c06b511ec3e8dc91a";
 
 $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=' + yourLocation + "&APPID=" + apikey, function(json_data){
 console.log(JSON.stringify(json_data));
+$('#data').val('Country:' +json_data.sys.country+"\nCity:" +json_data.name+"\nTimezone:"+json_data.timezone+"\nWeather:"+json_data.weather[1]+"\nWind:"+json_data.wind.speed);
 });
-
+}
 
 });
 });
