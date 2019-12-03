fd8f09ef4e25a65c61523fae6384b0ec444ab1e9
Dawid Dymarkowski <avacade2k@gmail.com>
2019-12-03 09:28:45 +0100

    .

--- script.js
+++ script.js
@@ -4,11 +4,10 @@
        var yourLocation = $("#city").val();
   $("#debug").append("You are searching for ... "+yourLocation+"\n");
 
-var dataString = "London";
 var apikey = "7b1c59b130e4e81c06b511ec3e8dc91a";
 
-$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=' + dataString + "&APPID=" + apikey, function(json_data){
-alert(JSON.stringify(json_data));
+$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=' + yourLocation + "&APPID=" + apikey, function(json_data){
+console.log(JSON.stringify(json_data));
 });
 
 
