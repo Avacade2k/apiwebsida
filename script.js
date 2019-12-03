60dad825333532b54e1e841f9947f4c7fa96b909
Dawid Dymarkowski <avacade2k@gmail.com>
2019-12-03 14:55:02 +0100

    g lol

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
         var apikey = "7b1c59b130e4e81c06b511ec3e8dc91a";
         $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=' + yourLocation + "&APPID=" + apikey, function(json_data){
             console.log(JSON.stringify(json_data));
-            $('#data').val('Country:' +json_data.sys.country+"\nCity:" +json_data.name+"\nTimezone:"+json_data.timezone+"\nWeather:"+json_data.weather[0].main+"\nWind:"+json_data.wind.speed+"\nTemperature:"+json_data.main.temp+"\nDescription: "+json_data.weather[0].description);
+            $('#data').val('Country:' +json_data.sys.country+"\nCity:" +json_data.name+"\nTimezone:"+json_data.timezone+"\nWeather:"+json_data.weather[0].main+"\nWind:"+json_data.wind.speed+"m/s\nTemperature:"+json_data.main.temp+"K\nDescription: "+json_data.weather[0].description);
             });
         }; 
     });
